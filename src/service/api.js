15bc6b7c74b38f1c02b566b597d12f8bd85e1aa2
NurlanAmanov <nurlanemenov14@gmail.com>
2025-02-08 15:34:20 +0400

    api v4

--- src/service/api.js
+++ src/service/api.js
@@ -2,21 +2,21 @@ import axios from "axios";
 
 
 async function GetData() {
-    const res = await axios.get("http://finalprojectt-001-site1.jtempurl.com/api/Category");
+    const res = await axios.get("https://finalprojectt-001-site1.jtempurl.com/api/Category");
     return res.data;
 }
 
 async function GetProduct() {
-    const res = await axios.get("http://finalprojectt-001-site1.jtempurl.com/api/Product");
+    const res = await axios.get("https://finalprojectt-001-site1.jtempurl.com/api/Product");
     return res.data;
 }
 
 async function getLocation() {
-    const res = await axios.get("http://finalprojectt-001-site1.jtempurl.com/api/Location")
+    const res = await axios.get("https://finalprojectt-001-site1.jtempurl.com/api/Location")
     return res.data
 }
 async function Getbanner() {
-    const res=await axios.get("http://finalprojectt-001-site1.jtempurl.com/api/Location")
+    const res=await axios.get("https://finalprojectt-001-site1.jtempurl.com/api/Location")
     return res.data;
     
 }
